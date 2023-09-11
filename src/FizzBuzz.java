68497d3008fbf1417c0f5148717b0a9ae9caf608
Terry Fu <terry.fu@mail.utoronto.ca>
2023-09-11 16:36:29 -0400

    Added more checkmarks, created new 'Main' file that calls fizz buzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    public static int dofizzbuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
