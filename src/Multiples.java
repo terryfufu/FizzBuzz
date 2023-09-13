a100e6c00283105182d63bec98c7d6cf93db75d6
Terry Fu <terry.fu@mail.utoronto.ca>
2023-09-12 22:53:24 -0400

    Finished the Multiples and Reduce method

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1; i < 1000; i++) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 || divisibleBy5) {
+                counter = counter + 1;
+            }
+        }
+        System.out.println(counter);
+    }
+}
